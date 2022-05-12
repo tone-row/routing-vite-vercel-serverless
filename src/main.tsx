0013a7a6ae0fa61774e77261211ea0913c62a68c
Rob Gordon <rchristopher.gordon@gmail.com>
2022-05-12 12:20:24 -0400

    Add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,57 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { BrowserRouter, Link, Outlet, Route, Routes } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/" element={<Index />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
+
+function App() {
+  return (
+    <div>
+      <nav style={{ display: "flex", gap: "1rem" }}>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
+
+function Index() {
+  return (
+    <main style={{ padding: "1rem 0" }}>
+      <h1>Index</h1>
+    </main>
+  );
+}
+
+function About() {
+  return (
+    <main style={{ padding: "1rem 0" }}>
+      <h1>About</h1>
+    </main>
+  );
+}
+
+function Contact() {
+  return (
+    <main style={{ padding: "1rem 0" }}>
+      <h1>Contact</h1>
+    </main>
+  );
+}
